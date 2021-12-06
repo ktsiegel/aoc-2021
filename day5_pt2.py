4b061718a58ba7db33f5167b456128d31b297a2d
Katie Siegel <katie.siegel@samsara.com>
2021-12-05 22:15:57 -0500

    finished day 5 pt 2

--- day5_pt2.py
+++ day5_pt2.py
@@ -19,31 +19,47 @@ def main():
 	# print(max_y)
 	
 	graph = []
-	for i in range(0, max_x+1):
+	for i in range(0, max_y+1):
 		row = []
-		for j in range(0, max_y+1):
+		for j in range(0, max_x+1):
 			row.append(0)
 		graph.append(row)
 		
 	# print(graph)
 	
 	for l in lines:
-		# only check horizontal or vertical
 		# print('line {}'.format(l))
 		if l[0][0] == l[1][0]:
 			## vertical line
 			start_y = min(l[0][1], l[1][1])
 			end_y = max(l[0][1], l[1][1])
 			for r in range(start_y, end_y + 1):
-				graph[l[0][0]][r] += 1
-		if l[0][1] == l[1][1]:
+				graph[r][l[0][0]] += 1
+		elif l[0][1] == l[1][1]:
 			## horizontal line
 			start_x = min(l[0][0],l[1][0])
 			end_x = max(l[0][0],l[1][0])
 			for c in range(start_x, end_x+1):
-				graph[c][l[0][1]] += 1
+				graph[l[0][1]][c] += 1
+		else:
+			# diagonal line
+			step_x = 1
+			step_y = 1
+			start_x = l[0][0]
+			end_x = l[1][0]
+			start_y = l[0][1]
+			end_y = l[1][1]
+			if start_x > end_x:
+				step_x = -1
+			if start_y > end_y:
+				step_y = -1
+			j = 0
+			for x_loc in range(start_x, end_x + step_x, step_x):
+				y_loc = start_y + (j*step_y)
+				graph[y_loc][x_loc] += 1
+				j += 1
 	for r in graph:
-		print(r)
+		print(' '.join([str(x) for x in r]).replace('0','.'))
 	
 		
 	count = 0
